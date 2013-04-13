6acd965d12c0370f4d65fea7e7e582a593fb8861
Henrik Lundstedt <hlundstedt@macadamian.com>
2013-04-13 17:38:43 -0400

    - Added server callback functions - Using array buffer inste of Queue

--- t9/assets/js/BusStopManager.js
+++ t9/assets/js/BusStopManager.js
@@ -1,29 +1,60 @@
-function BusStopManager() {
+var BusStopManager = (function() {
 
     var BUFFER_SIZE = 10;
+    var INITIAL_STOP_TIME = 100;
 
-    this.busNumbers = {};
-    this.busStops = {};
+    var buffer = {};
 
-    var socket = io.connect('http://localhost');
+    var currentStart = 0;
+    var currentStop = INITIAL_STOP_TIME;
+    var busNumbers = {};
+    var busNumberEnabled = {};
+    var busStops = {};
+    var socket;
 
-    this.getBusNumbers = function() {
-        return this.busNumbers;
+    var self = function() {
+        socket = io.connect('http://localhost');
+        socket.on('onBusNumbersReceived', self.onBusNumbersRecieved());
+        socket.on('onBusStopsReceived', self.onBusStopsReceived());
     };
 
-    this.getNextBusStop = function(busNumber) {
+    // --- Interface ---
+
+    self.getBusNumbers = function() {
+        return busNumbers;
+    };
+
+    self.enableBusNumber = function(busNumber, enable) {
+        if (enable) {
+            busNumbers.insert(busNumber);
+        } else {
+            delete busNumbers[busNumbers];
+        }
+    };
+
+    self.getBusStops = function(time) {
         if (bustStops[busNumber]) {
             return busStops[busNumber].dequeue();
         }
     };
 
-    this.onNewBusStopFetched = function(busStop) {
-        if (!busStops[busStop.busNumber]) {
-            busStops[busStop.busNumber] = new Queue();
-        }
-        if (busStops[bustStop.busNumber].getLength() < BUFFER_SIZE) {
-            busStops[busStop.busNumber].enqueue(busStop);
+    // --- Server callbacks ---
+
+    self.onBusNumbersReceived = function(aBusNumbers) {
+        busNumbers = aBusNumbers;
+        socket.emit('getBusStops', { start: currentStart, end: currentStop });
+    };
+
+    self.onBusStopsReceived = function(data) {
+        for (var i=0; i<data.length; i++) {
+            buffer[data[i].time] = {
+                routeNumber: data[i].routeNumber,
+                busNumber: data[i].busNumber,
+                x: data[i].x,
+                y: data[i].y,
+                nextTime: data[i].nextTime
+            };
         }
     };
 
-}
+})();
