081b12caab22ab526c75ffebade612edcd754655
Jake Tober <jake@macadamian.com>
2013-04-13 13:53:41 -0400

    Adds tool to transform the data into a usable format

---
+++ DataTransformer/DataTransform/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DataTransform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Process = new System.Windows.Forms.Button();
+            this.richTextBox_Output = new System.Windows.Forms.RichTextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Process
+            // 
+            this.button_Process.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Process.Location = new System.Drawing.Point(12, 38);
+            this.button_Process.Name = "button_Process";
+            this.button_Process.Size = new System.Drawing.Size(270, 23);
+            this.button_Process.TabIndex = 0;
+            this.button_Process.Text = "Process New Data";
+            this.button_Process.UseVisualStyleBackColor = true;
+            this.button_Process.Click += new System.EventHandler(this.button_Process_Click);
+            // 
+            // richTextBox_Output
+            // 
+            this.richTextBox_Output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_Output.Location = new System.Drawing.Point(12, 67);
+            this.richTextBox_Output.Name = "richTextBox_Output";
+            this.richTextBox_Output.Size = new System.Drawing.Size(260, 183);
+            this.richTextBox_Output.TabIndex = 1;
+            this.richTextBox_Output.Text = "";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(123, 10);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(149, 20);
+            this.textBox_name.TabIndex = 2;
+            this.textBox_name.Text = "ottawa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.richTextBox_Output);
+            this.Controls.Add(this.button_Process);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Process;
+        private System.Windows.Forms.RichTextBox richTextBox_Output;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
